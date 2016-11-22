6a6e6800e0663dc4633629f1e1148643c490ea33
philxchen <xchen200@uottawa.ca>
2016-11-22 00:28:45 -0500

    get some button images

---
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/BrowseIngredientActivity.java
@@ -0,0 +1,14 @@
+package ca.uottawa.cookingwithgarzon;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BrowseIngredientActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_ingredient);
+    }
+
+}
--- app/src/main/java/ca/uottawa/cookingwithgarzon/MainActivity.java
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/MainActivity.java
@@ -1,13 +1,23 @@
 package ca.uottawa.cookingwithgarzon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mRecipeButton = (Button) findViewById(R.id.mRecipeButton);
+        mRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
