c8c4a84e20b537fc6ac38fbffabf03a439ee3179
Jonathan Gratton <jonathan.gratton@gmail.com>
2016-11-24 15:59:43 -0500

    Added all activities for the main application.

---
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/RecipeDifficulty.java
@@ -0,0 +1,13 @@
+package ca.uottawa.cookingwithgarzon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeDifficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_difficulty);
+    }
+}
