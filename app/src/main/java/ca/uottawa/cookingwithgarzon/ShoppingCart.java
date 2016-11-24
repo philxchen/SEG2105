0be843d47af145d98de8e97a6d712435dfc0699c
Jonathan Gratton <jonathan.gratton@gmail.com>
2016-11-24 15:59:43 -0500

    Added all activities for the main application.

---
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/ShoppingCart.java
@@ -0,0 +1,13 @@
+package ca.uottawa.cookingwithgarzon;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ShoppingCart extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+}
