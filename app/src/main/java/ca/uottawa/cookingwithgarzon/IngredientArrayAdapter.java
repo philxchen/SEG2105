1e4db9b8ff5c0ec118552acd4ce2c4909801f614
philxchen <xchen200@uottawa.ca>
2016-11-27 17:29:24 -0500

    Fixed IngredientArrayAdapter

--- app/src/main/java/ca/uottawa/cookingwithgarzon/IngredientArrayAdapter.java
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/IngredientArrayAdapter.java
@@ -8,26 +8,30 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import ca.uottawa.cookingwithgarzon.model.Ingredient;
+
 /**
  * Created by philxchen on 27/11/16.
  */
 
 public class IngredientArrayAdapter extends ArrayAdapter<String> {
     private final Context context;
-    private final String[] names;
+    private final ArrayList<Ingredient> ingredients;
 
-    public IngredientArrayAdapter(Context context, String[] names) {
+    public IngredientArrayAdapter(Context context, ArrayList<Ingredient> ingredients) {
         super(context, R.layout.content_shopping_cart);
         this.context = context;
-        this.names = names;
+        this.ingredients = ingredients;
     }
 
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.content_shopping_cart, parent, false);
+        View rowView = inflater.inflate(R.layout.shopping_cart_list_item, parent, false);
         TextView nameTextView = (TextView) rowView.findViewById(R.id.ingredient_name);
-        nameTextView.setText(names[position]);
+        nameTextView.setText(ingredients.get(position).get_name());
         return rowView;
     }
 }
