ef2f93e6e71b06bf7aa3d0bfda939d86f2310ce4
Joel Faubert <joelfaubert@gmail.com>
2016-11-27 18:10:42 -0500

    test

--- app/src/main/java/ca/uottawa/cookingwithgarzon/CreateIngredient.java
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/CreateIngredient.java
@@ -1,11 +1,14 @@
 package ca.uottawa.cookingwithgarzon;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,13 +21,11 @@ public class CreateIngredient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_ingredient);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         final Button saveBtn = (Button) findViewById(R.id.saveIngredientBtn);
         final EditText nameTxt = (EditText) findViewById(R.id.ingredientTxt);
         final EditText priceTxt = (EditText) findViewById(R.id.priceTxt);
-        
+
         View.OnClickListener oclSaveBtn= new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,20 +41,6 @@ public class CreateIngredient extends AppCompatActivity {
 
         saveBtn.setOnClickListener(oclSaveBtn);
 
-
-
-
-
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
