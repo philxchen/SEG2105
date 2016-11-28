1892426295e17626c702700a50aa5b2dac775bc5
philxchen <cxuankai@outlook.com>
2016-11-28 15:13:45 -0500

    Created Help and added titles for activities.

---
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/HelpActivity.java
@@ -0,0 +1,13 @@
+package ca.uottawa.cookingwithgarzon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
