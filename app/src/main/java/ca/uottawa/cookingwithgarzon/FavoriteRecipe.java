1e3f522068d02f47c79c85f0346c2f1c95995944
Francisco <francisco.ad.trindade@gmail.com>
2016-11-28 22:49:47 -0500

    Updated and fixed error with Favourite Recipe

--- app/src/main/java/ca/uottawa/cookingwithgarzon/FavoriteRecipe.java
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/FavoriteRecipe.java
@@ -1,11 +1,12 @@
 package ca.uottawa.cookingwithgarzon;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavoriteRecipe extends AppCompatActivity {
 
@@ -13,16 +14,7 @@ public class FavoriteRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_recipe);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
+
