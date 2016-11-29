6ac944a66d9c6d4edd4ef200a4836348cd6e0dcc
Francisco <francisco.ad.trindade@gmail.com>
2016-11-28 20:42:39 -0500

    Quick fix to toolbar in CreateRecipe activity

--- app/src/main/java/ca/uottawa/cookingwithgarzon/CreateRecipe.java
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/CreateRecipe.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import ca.uottawa.cookingwithgarzon.adapter.HintAdapter;
 
