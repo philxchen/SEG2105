ec3f8c89b38a66957d9e7276afc4659d7d141678
philxchen <xchen200@uottawa.ca>
2016-11-28 21:18:51 -0500

    fixed crash by shopping cart

--- app/src/main/java/ca/uottawa/cookingwithgarzon/ShoppingCartActivity.java
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/ShoppingCartActivity.java
@@ -18,8 +18,6 @@ public class ShoppingCartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
