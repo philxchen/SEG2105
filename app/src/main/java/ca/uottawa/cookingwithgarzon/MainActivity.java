0df8ec0b8a18be6a6284b68cbdb1093ad53d63f8
WangAlice123 <wangalice731@gmail.com>
2016-12-02 16:35:03 -0500

    Added Help button for main activity

--- app/src/main/java/ca/uottawa/cookingwithgarzon/MainActivity.java
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.design.widget.FloatingActionButton;
 
 import ca.uottawa.cookingwithgarzon.helper.DbHelper;
 
@@ -20,6 +21,8 @@ public class MainActivity extends Activity {
         final Button favouritesBtn = (Button) findViewById(R.id.favouritesBtn);
         final Button shoppingListBtn = (Button) findViewById(R.id.shoppingCartBtn);
         final Button clearDbBtn = (Button) findViewById(R.id.clearDbBtn);
+        final FloatingActionButton helpBtn = (FloatingActionButton) findViewById(R.id.helpBtn);
+
 
         View.OnClickListener oclSearchBtn = new View.OnClickListener() {
             @Override
@@ -55,6 +58,15 @@ public class MainActivity extends Activity {
                 DbHelper.clearDatabase(getApplicationContext());
             }
         };
+
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent helpIntent = new Intent(MainActivity.this, HelpActivity.class);
+                helpIntent.putExtra("helpTextName", "recipeSearchHelp");
+                startActivity(helpIntent);
+            }
+        });
 
         searchBtn.setOnClickListener(oclSearchBtn);
         createBtn.setOnClickListener(oclCreateBtn);
