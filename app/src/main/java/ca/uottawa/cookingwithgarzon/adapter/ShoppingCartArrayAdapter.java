03f457a0f855de3b06f8b5eaf98fa243aa5ab2fb
philxchen <cxuankai@outlook.com>
2016-12-01 23:06:17 -0500

    implemented display and db of shopping cart

---
+++ app/src/main/java/ca/uottawa/cookingwithgarzon/adapter/ShoppingCartArrayAdapter.java
@@ -0,0 +1,10 @@
+package ca.uottawa.cookingwithgarzon.adapter;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by philxchen on 12/1/16.
+ */
+
+public class ShoppingCartArrayAdapter extends ArrayAdapter {
+}
